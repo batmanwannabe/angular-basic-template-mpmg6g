6fce5c70f02223ca8860d62a0a2482cd84919e8e
batmanwannabe <solomonsahayaraj911@gmail.com>
2021-07-27 09:34:45 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { LocationStrategy, PlatformLocation } from '@angular/common';
 import { Router, UrlSerializer } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'hello',
-  template: `
-  `,
+  template: ``
 })
 export class HelloComponent {
-  constructor(private router: Router, private serializer: UrlSerializer) {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router,
+    private serializer: UrlSerializer
+  ) {
     const queryParams = { foo: 'a', bar: 42 };
     const tree = router.createUrlTree([], { queryParams });
 
     console.log(serializer.serialize(tree));
+
+    console.log(this.document.location.href);
   }
 }
