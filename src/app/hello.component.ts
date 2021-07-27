a13565c1129e42ce7ef99cc79020a89a318e4dc6
batmanwannabe <solomonsahayaraj911@gmail.com>
2021-07-27 09:43:26 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -18,6 +18,11 @@ export class HelloComponent {
 
     console.log(serializer.serialize(tree));
 
+    //it isn't recommended to use window directly with Angular as it not available on the server side when pre-rendering so will break Angular Universal (important if you are planning on having an SEO friendly site)
     console.log(this.document.location.href);
+
+    console.log(this.document.location.hostname);
+
+    console.log(this.document.location.origin);
   }
 }
